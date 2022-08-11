eb57473fc4a257215ed0fa0abf1d6b074472994e
Tristan J. McNab <tristan.j.mcnab@gmail.com>
2022-08-10 21:18:46 -0400

    improve parsing process

--- src/chapters/04/code2/lib/Parser.ts
+++ src/chapters/04/code2/lib/Parser.ts
@@ -6,18 +6,70 @@ export namespace Parser {
 		NULL     = 'NULL',
 		SELECT   = 'SELECT',
 		TRUE     = 'TRUE',
-		WILDCARD = '*',
+		WHERE    = 'WHERE',
+	}
+
+	export enum Operator {
+		EQUAL     = '=',
+		NOT_EQUAL = '≠',
 	}
 
 	export enum Type {
-		KEYWORD = 'KEYWORD',
-		UNKNOWN = 'UNKNOWN',
-		VALUE   = 'VALUE',
+		IDENTIFIER = 'IDENTIFIER',
+		KEYWORD    = 'KEYWORD',
+		OPERATOR   = 'OPERATOR',
+		UNKNOWN    = 'UNKNOWN',
+		VALUE      = 'VALUE',
 	}
 	
 	export type Value = boolean | null | number | string
 	
 	export type Token = [Type, (Keyword | Value)?]
+
+	export const isIdentifier = (input: string) : boolean => {
+		const matcher = /^[a-z].[a-z]+$|^[a-z]+$/gmi
+		return input.match(matcher)?.length === 1
+	}
+
+	export const matchedIdentifier = (input: string, tokens: Token[]): boolean => {
+		const matcher = /^[a-z].[a-z]+$|^[a-z]+$/gmi
+		
+		if (input.match(matcher)?.length === 1) {
+			tokens.push([Type.IDENTIFIER, input])
+			return true
+		}
+		
+		return false
+	}
+
+	export const matchedKeyword = (input: string, tokens: Token[]) : boolean => {
+		const inputAsUpperCase = input.toUpperCase()
+		
+		if (inputAsUpperCase in Keyword) {
+			const value : Keyword = (<any>Keyword)[inputAsUpperCase]
+			tokens.push([Type.KEYWORD, value])
+			return true
+		}
+		
+		return false
+	}
+
+	export const matchedNumber = (input: string, tokens: Token[]) : boolean => {
+		
+		const asInt = Number.parseInt(input)
+		if (!Number.isNaN(asInt)) {
+			tokens.push([Parser.Type.VALUE, asInt])
+			return true
+		}
+		
+		const asFloat = Number.parseFloat(input)
+		if (!Number.isNaN(asFloat)) {
+			tokens.push([Parser.Type.VALUE, asFloat])
+			return true
+		}
+
+		return false
+	}
 	
 	export const parse = (input: string) : Parser.Token[] => {
 		const elements = input.split(/\s+/gm)
@@ -25,27 +77,14 @@ export namespace Parser {
 		
 		for (let index = 0; index < elements.length; index += 1) {
 			const element = elements[index]
-			const elementToUpper = element.toUpperCase()
 
-			const asInt = Number.parseInt(element)
-			if (!Number.isNaN(asInt)) {
-				tokens.push([Parser.Type.VALUE, asInt])
-				continue
-			}
+			if (matchedNumber(element, tokens)) continue
 
-			const asFloat = Number.parseFloat(element)
-			if (!Number.isNaN(asFloat)) {
-				tokens.push([Parser.Type.VALUE, asFloat])
-				continue
-			}
+			if (matchedKeyword(element, tokens)) continue
 
-			if (elementToUpper in Keyword) {
-				const value : Keyword = (<any>Keyword)[elementToUpper]
-				tokens.push([Type.KEYWORD, value])
-				continue
-			}
+			if (matchedIdentifier(element, tokens)) continue
 
-			tokens.push([Parser.Type.UNKNOWN])
+			tokens.push([Parser.Type.UNKNOWN, element])
 		}
 
 		return tokens
--- src/chapters/04/code2/test/Parser.ts
+++ src/chapters/04/code2/test/Parser.ts
@@ -2,7 +2,7 @@ import { testFn } from './runner'
 import { Parser } from '../lib/Parser'
 
 export const test = (test: testFn) => {
-	const input = `SELECT * FROM users u WHERE u.id = 5`
+	const input = `SELECT FROM users u WHERE u.id = 5`
 	const result = Parser.parse(input)
 	console.log(input, result)
 }
