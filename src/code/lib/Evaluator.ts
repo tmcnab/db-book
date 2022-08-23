1b4bbe0b09af9f454deaff9688d9a65758925de4
Tristan J. McNab <tristan.j.mcnab@gmail.com>
2022-08-22 23:10:54 -0400

    remove duplicate code dir

--- src/code/lib/Evaluator.ts
+++
@@ -1,9 +0,0 @@
-import { Parser } from './Parser'
-
-export namespace Executor {
-
-	export const execute = (statements: Parser.Token[], buffer: ArrayBuffer): boolean[] => {
-		return []
-	}
-
-}
--- src/code/lib/main.ts
+++
@@ -1,7 +0,0 @@
-import { Parser } from './Parser'
-
-export default (input: string) : [] => {
-	const parse = Parser.parse(input)
-	console.log(parse)
-	return []
-}
